dd08fc3aece35781fa5b1d21db130a56c5038612
Josh Archer <jarcher@greenriver.edu>
2022-05-11 14:52:14 -0700

    Added an example JPA configuration.

---
+++ jpa_project/src/main/java/edu/greenriver/sdev/jpa_project/JpaProjectApplication.java
@@ -0,0 +1,15 @@
+package edu.greenriver.sdev.jpa_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JpaProjectApplication.class, args);
+    }
+
+}
